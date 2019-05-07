a1416f74626babd12f3fb66160cf71f608e61e3c
lynnexzhou <lynnexzhou@tencent.com>
2019-05-07 11:23:24 +0800

    Update cell.py

--- cell.py
+++ cell.py
@@ -2,6 +2,25 @@
 import tensorflow as tf
 import numpy as np
 
+def orthogonal(shape):
+    flat_shape = (shape[0], np.prod(shape[1:]))
+    a = np.random.normal(0.0, 1.0, flat_shape)
+    u, _, v = np.linalg.svd(a, full_matrices=False)
+    q = u if u.shape == flat_shape else v
+    return q.reshape(shape)
+
+def lstm_ortho_initializer(scale=1.0):
+    def _initializer(shape, dtype=tf.float32, partition_info=None):
+        size_x = shape[0]
+        size_h = shape[1]/4 # assumes lstm.
+        t = np.zeros(shape)
+        t[:, :size_h] = orthogonal([size_x, size_h])*scale
+        t[:, size_h:size_h*2] = orthogonal([size_x, size_h])*scale
+        t[:, size_h*2:size_h*3] = orthogonal([size_x, size_h])*scale
+        t[:, size_h*3:] = orthogonal([size_x, size_h])*scale
+        return tf.constant(t, dtype)
+  return _initializer
+
 class LSTMCell(tf.contrib.rnn.RNNCell):
     def __init__(self, num_units, RL, forget_bias=1.0, use_layer_norm=False,
     use_recurrent_dropout=False, dropout_keep_prob=1.0):
@@ -14,7 +33,16 @@ class LSTMCell(tf.contrib.rnn.RNNCell):
         self.cur_time =0
 
     def get_newch(self, state, x, cur_time, RL):
+        self.C_state = tf.concat([self.C_state[1:], tf.expand_dims(state[0],0)], 0)
+        self.H_state = tf.concat([self.H_state[1:], tf.expand_dims(state[1],0)], 0)
+        RL_input = tf.concat([x, tf.reduce_mean(self.H_state, 0)], 1)
+        actions = self.RL.choose_action(RL_input, cur_time)
+        coord = tf.concat([tf.cast(actions, tf.int32), tf.expand_dims(tf.range(tf.shape(actions)[0]),1)],1)
 
+        self.c_state = tf.gather_nd(self.C_state, coord)
+        self.h_state = tf.gather_nd(self.H_state, coord)
+
+        return self.c_state, self.h_state
 
     def __call__(self, x, state, scope = None):
         with tf.variable_scope(scope or type(self).__name__):
@@ -33,3 +61,8 @@ class LSTMCell(tf.contrib.rnn.RNNCell):
                 self.H_state = tf.reshape(tf.tile(tf.reshape(tf.zeros_like(h, dtype=tf.float32),[-1]),[self.RL.n_actions]), [self.RL.n_actions, batch_size, self.num_units])
             else:
                 c, h = self.get_newch(state, x, cur_time, self.RL)
+
+            h_size = self.num_units
+            w_init=None # uniform
+
+            h_init=lstm_ortho_initializer()
